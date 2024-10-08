3ddddb3f8871f0162b279af68ce8e3f42e185f5d
thanoskor <thanoskorovilas@gmail.com>
2024-10-08 20:12:39 +0300

    '

--- square.py
+++ square.py
@@ -2,9 +2,10 @@ import pygame
 from pieces import *
 
 class square:
-    
+
     def __init__(self, tile_size, color) -> None:
         self.color = 'color'
-    
+    """
     def draw_self(self, window):
         pygame.draw.rect(window, color, (col * self.tile_size, row * self.tile_size, self.tile_size, self.tile_size))
+    """
