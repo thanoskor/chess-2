1a2c908f8ce85f446c37d9f0b5d487d7e3f7b836
thanoskor <thanoskorovilas@gmail.com>
2024-10-09 18:18:33 +0300

    added event handling to pawns

--- Game.py
+++ Game.py
@@ -3,22 +3,18 @@ import sys
 from UI import *
 from pieces import *
 
-Board = [[None for i in range(8)]for i in range(8)]
-
-for piece in ALL_PIECES:
-    Board[piece.row][piece.col] = piece
-
-
 ui = UI()
 class GAME:
-    def __init__(self) -> None:
-        pass
-    
+    def __init__(self, fps) -> None:
+        self.frame_rate = fps
+        self.clock = pygame.time.Clock()
+
     def get_square(self, mousep):
         return mousep[0]//TILE_SIZE, mousep[1]//TILE_SIZE
 
     def start(self): 
-        while True:
+        event_receiving_pieces = []
+        while True:  
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -28,11 +24,20 @@ class GAME:
                     x, y = self.get_square(pygame.mouse.get_pos())
                     if Board[x][y]:
                         Board[x][y].got_clicked()
+                        event_receiving_pieces.append(Board[x][y])
+                
+                for piece in event_receiving_pieces:
+                    piece.handle_events(event)
             
+            for piece in event_receiving_pieces:
+                if piece.receiving_events == False:
+                    event_receiving_pieces.remove(piece)
+
             pygame.display.flip()         
             ui.draw_board()
             ui.draw_pieces(ALL_PIECES)
             ui.update()
+            self.clock.tick(self.frame_rate)
 
-game = GAME()
+game = GAME(60)
 game.start()
--- pieces.py
+++ pieces.py
@@ -1,3 +1,7 @@
+import pygame
+from UI import TILE_SIZE
+
+Board = [[None for i in range(8)]for i in range(8)]
 ALL_PIECES = []
 
 class Piece:
@@ -6,9 +10,23 @@ class Piece:
         self.col = col
         self.color = color
         ALL_PIECES.append(self)
+        self.receiving_events = False
 
     def got_clicked(self):
-        print("!!")
+        self.receiving_events = True
+
+    def get_square(self, mousep):
+        return mousep[0]//TILE_SIZE, mousep[1]//TILE_SIZE
+
+    def handle_events(self, event):
+        print(event)
+        if event.type == pygame.MOUSEBUTTONUP:
+            x, y = self.get_square(pygame.mouse.get_pos())
+            Board[self.row][self.col] = None
+            self.row = x
+            self.col = y
+            Board[x][y] = self
+            self.receiving_events = False
 
 class Pawn(Piece):
     def __init__(self, row, col, color) -> None:
@@ -16,3 +34,6 @@ class Pawn(Piece):
         self.type = "p"
 
 Pawn(1,1,"w")
+
+for piece in ALL_PIECES:
+    Board[piece.row][piece.col] = piece
